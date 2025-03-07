2b26317132799ff4e7b04e2028219fb186a4abfb
akshat-technomark <119396974+akshat-technomark@users.noreply.github.com>
2025-03-07 15:40:57 +0530

    required env changes

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const fetchPageData = async (slug: string) => {
   try {
-    const response = await axios.get(`http://localhost:3000/pages/${slug}.json`);
+    const response = await axios.get(`${API_BASE_URL}/pages/${slug}.json`);
     return response.data;
   } catch (error) {
     console.error("Error fetching page data:", error);
